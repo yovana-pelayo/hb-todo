6775fe55028b7c65ff9f8ed704d3adc03551e920
yovana-pelayo <94511266+yovana-pelayo@users.noreply.github.com>
2022-02-16 11:30:48 -0800

    Initial commit

---
+++ render-utils.js
@@ -0,0 +1,12 @@
+export function renderTodo(todo) {
+    // create a div and a p tag
+    // depending on whether the todo is complete, give the div the appropriate css class ('complete' or 'incomplete')
+
+    // add the 'todo' css class no matter what
+
+    // put the todo's text into the p tag
+
+    // append stuff
+
+    // return the div
+}
---
+++ todos/todos.js
@@ -0,0 +1,43 @@
+import { 
+    checkAuth, 
+    createTodo, 
+    completeTodo,
+    getTodos,
+    logout,
+    deleteAllTodos, 
+} from '../fetch-utils.js';
+import { renderTodo } from '../render-utils.js';
+
+checkAuth();
+
+const todosEl = document.querySelector('.todos');
+const todoForm = document.querySelector('.todo-form');
+const logoutButton = document.querySelector('#logout');
+const deleteButton = document.querySelector('.delete-button');
+
+todoForm.addEventListener('submit', async(e) => {
+    // on submit, create a todo, reset the form, and display the todos
+});
+
+async function displayTodos() {
+    // fetch the todos
+    
+    // display the list of todos
+
+    // be sure to give each todo an event listener
+
+    // on click, complete that todo
+}
+
+// add an on load listener that fetches and displays todos on load
+
+logoutButton.addEventListener('click', () => {
+    logout();
+});
+
+
+deleteButton.addEventListener('click', async() => {
+    // delete all todos
+
+    // then refetch and display the updated list of todos
+});
